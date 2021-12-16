b8b0ba943e407422cfab2b7d36b60cfad5b9691b
Ananya <avagadi15@gmail.com>
2021-12-16 20:42:57 +0530

    adin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,15 @@ class Ability
     
       user ||= User.new # guest user (not logged in)
         can :manage, User, id: user.id
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    #   
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can [:show, :edit, :update], User, id: user.id
+      can :create, Comment, user_id: user.id
+      can [:show, :index], Post
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
