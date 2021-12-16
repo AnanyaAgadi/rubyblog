0d277cce3235c5afeb171e7a4e09a54686c6cdd4
Ananya <avagadi15@gmail.com>
2021-12-16 20:26:33 +0530

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'pages#index'  
+  root 'posts#index'  
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
