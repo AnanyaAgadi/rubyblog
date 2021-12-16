924f03146c17ae5b2c4ebea8b214401ac9cdc87e
Ananya <avagadi15@gmail.com>
2021-12-16 16:54:36 +0530

    adding posts & styling

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_080455) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.datetime "date"
+    t.date "date"
     t.string "author"
     t.text "description"
     t.datetime "created_at", null: false
